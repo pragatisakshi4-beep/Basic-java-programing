fb651d13bf43a2bb0b376ddca90379e8a32908d8
pragatisakshi <275427708+pragatisakshi4-beep@users.noreply.github.com>
2026-08-17 17:24:15 +0530

    all changes

---
+++ Basic-programing/Switch.java
@@ -0,0 +1,43 @@
+public class Switch {
+    public static void main(String[] args) {
+
+        char grade = 'A';
+
+        switch (grade) {
+
+            case 'A':
+                System.out.println("Grade: A");
+                System.out.println("Performance: Excellent");
+                System.out.println("Keep it up!");
+                break;
+
+            case 'B':
+                System.out.println("Grade: B");
+                System.out.println("Performance: Very Good");
+                System.out.println("Good work!");
+                break;
+
+            case 'C':
+                System.out.println("Grade: C");
+                System.out.println("Performance: Good");
+                System.out.println("Need some improvement.");
+                break;
+
+            case 'D':
+                System.out.println("Grade: D");
+                System.out.println("Performance: Average");
+                System.out.println("Work harder.");
+                break;
+
+            case 'F':
+                System.out.println("Grade: F");
+                System.out.println("Result: Fail");
+                System.out.println("Improve your performance.");
+                break;
+
+            default:
+                System.out.println("Invalid Grade");
+        }
+    }
+}
+
