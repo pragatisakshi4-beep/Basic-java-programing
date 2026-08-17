f3ab3f349658dcb82e09a7a83fde25d6ae8ccae0
pragatisakshi <275427708+pragatisakshi4-beep@users.noreply.github.com>
2026-08-17 18:06:11 +0530

    add

---
+++ Basic-programing/NastedLoop.java
@@ -0,0 +1,16 @@
+public class NastedLoop {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 3; i++) {
+
+            System.out.println("Table of " + i);
+
+            for (int j = 1; j <= 10; j++) {
+
+                System.out.println(i + " x " + j + " = " + (i * j));
+            }
+
+            System.out.println("----------------");
+        }
+    }
+}
