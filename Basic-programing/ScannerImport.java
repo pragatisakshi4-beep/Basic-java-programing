f3895bc0eb9abe923cfe9fe92afef967b4287cb1
pragatisakshi <275427708+pragatisakshi4-beep@users.noreply.github.com>
2026-08-17 18:14:31 +0530

    add

---
+++ Basic-programing/ScannerImport.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ScannerImport {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String firstName = sc.next();
+        int solvedProblems = sc.nextInt();
+        double assessmentPercentage = sc.nextDouble();
+
+        System.out.println("Learner: " + firstName);
+        System.out.println("Problems solved: " + solvedProblems);
+        System.out.println("Assessment: " + assessmentPercentage);
+    }
+}
