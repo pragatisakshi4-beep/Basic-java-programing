a3b149dce87a67c61dda4b222db5cf10bd023216
pragatisakshi <275427708+pragatisakshi4-beep@users.noreply.github.com>
2026-08-17 16:31:06 +0530

    add all changes

---
+++ Basic-programing/WeeklyPreparationPlan.java
@@ -0,0 +1,20 @@
+
+public class WeeklyPreparationPlan {
+    public static void main(String[] args) {
+
+        int javaHoursPerDay = 2;
+        int aptitudeHoursPerDay = 1;
+        int numberOfDays = 5;
+
+        int weeklyJavaHours = javaHoursPerDay * numberOfDays;
+        int weeklyAptitudeHours = aptitudeHoursPerDay * numberOfDays;
+
+        int totalPreparationHours =
+                weeklyJavaHours + weeklyAptitudeHours;
+
+        System.out.println("Java: " + weeklyJavaHours);
+        System.out.println("Aptitude: " + weeklyAptitudeHours);
+        System.out.println("Total: " + totalPreparationHours);
+    }
+}
+
